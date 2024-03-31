eedc25f2b09be132049570b1584eccdd8e0e59a2
ccf19881030@gmail.com <ccf19881030@gmail.com>
2024-03-31 22:22:42 +0800

    Intro to gRPC in C# - How To Get Started

---
+++ GrpcGreeter/Services/CustomerService.cs
@@ -0,0 +1,76 @@
+﻿using Grpc.Core;
+using GrpcGreeter;
+
+namespace GrpcGreeter.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    { 
+        private readonly ILogger<CustomerService> _logger;
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            var customerId = request.UserId;
+            var customerModel = new CustomerModel();
+            if (customerId == 1)
+            {
+                customerModel.FirstName = "Steve";
+                customerModel.LastName = "Jobs";
+            } else if (customerId == 2)
+            {
+                customerModel.FirstName = "Bill";
+                customerModel.LastName = "Gates";
+            } else if (customerId == 3)
+            {
+                customerModel.FirstName = "Ami";
+                customerModel.LastName = "Smith";
+            }
+            return Task.FromResult(customerModel);
+        }
+
+        public override async Task GetNewCustomers(
+            NewCustomerRequest request,
+            IServerStreamWriter<CustomerModel> responseStream,
+            ServerCallContext context)
+        {
+            //return base.GetNewCustomers(request, responseStream, context);
+            // 下面使用列表模拟实际数据，实际生产环境中一般采用数据库存储数据
+            List<CustomerModel> customers = new List<CustomerModel>
+            {
+                new CustomerModel
+                {
+                    FirstName = "Tim",
+                    LastName = "Corey",
+                    EmailAddress = "tim@iamtimcorey.com",
+                    Age = 41,
+                    IsAlive = true
+                },
+                new CustomerModel
+                {
+                    FirstName = "Sue",
+                    LastName = "Storm",
+                    EmailAddress = "sue@stormy.net",
+                    Age = 28,
+                    IsAlive = false
+                },
+                new CustomerModel
+                {
+                    FirstName = "Bilbo",
+                    LastName = "Baggins",
+                    EmailAddress = "bilbo@middleearth.net",
+                    Age = 117,
+                    IsAlive = false
+                }
+            };
+
+            foreach (var customerItem in customers)
+            {
+                // await Task.Delay(1000); // 延时1秒
+                await responseStream.WriteAsync(customerItem);
+            } 
+        }
+    }
+}
---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+// See https://aka.ms/new-console-template for more information
+
+// The port number must match the port of the gRPC server.
+//using var channel = GrpcChannel.ForAddress("https://localhost:7223");
+//var client = new Greeter.GreeterClient(channel);
+//var reply = await client.SayHelloAsync(
+//                  new HelloRequest{ Name = "GreeterClient" });
+//Console.WriteLine("Greeeting: " + reply.Message);
+
+//reply = await client.SayHelloAgainAsync(
+//              new HelloRequest { Name = "GreeterClient Async " });
+//Console.WriteLine("Greeeting: " + reply.Message);
+
+var channel = GrpcChannel.ForAddress("https://localhost:7223");
+var customerClient = new Customer.CustomerClient(channel);
+var clientRequestd = new CustomerLookupModel { UserId = 2 };
+var customer = await customerClient.GetCustomerInfoAsync(clientRequestd);
+Console.WriteLine($"{customer.FirstName } {customer.LastName}");
+
+Console.WriteLine();
+Console.WriteLine("New Customer List");
+Console.WriteLine();
+
+using (var call = customerClient.GetNewCustomers(new NewCustomerRequest()))
+{
+    while (await call.ResponseStream.MoveNext())
+    {
+        var currentCustomer = call.ResponseStream.Current;
+        Console.WriteLine($"{currentCustomer.FirstName} { currentCustomer.LastName} { currentCustomer.EmailAddress }");
+    }
+}
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+
